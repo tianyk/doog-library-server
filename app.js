4dfe6186e32a4f80d0914f681a8f8d95582110de
tyk <yongketian@gmail.com>
2015-07-02 00:11:19 +0800

    根据系统环境配置日志

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
-var FileStreamRotator = require('file-stream-rotator');
-var mkdirp = require('mkdirp');
+
 var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -25,24 +24,30 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// HTTP日志
-var logDirectory = config.logDir;
-// ensure log directory exists
-mkdirp.sync(logDirectory);
-var accessLogStream = FileStreamRotator.getStream({
-    filename: logDirectory + '/access-%DATE%.log',
-    frequency: 'daily',
-    verbose: false, // 开发模式下打开
-    date_format: 'YYYY-MM-DD'
-});
+if (app.get('env') === 'production') {
+    var FileStreamRotator = require('file-stream-rotator');
+    var mkdirp = require('mkdirp');
 
-morgan.token('sid', function(req) {
-    return req.sessionID || 'NoSessionID';
-});
-// [日期] "请求方法 请求URL 响应码 响应时间 ms HTTP协议版本" "SessionID" 客户端IP 远端用户 响应体大小 "响应头referrer" "浏览器UA"
-app.use(morgan('[:date[iso]] ":method :url :status :response-time ms - HTTP/:http-version" ":sid" :remote-addr - :remote-user :res[content-length] ":referrer" ":user-agent"', {
-    stream: accessLogStream
-}));
+    // HTTP日志
+    var logDirectory = config.logDir;
+    // ensure log directory exists
+    mkdirp.sync(logDirectory);
+    var accessLogStream = FileStreamRotator.getStream({
+        filename: logDirectory + '/access-%DATE%.log',
+        frequency: 'daily',
+        verbose: false, // 开发模式下打开
+        date_format: 'YYYY-MM-DD'
+    });
+    morgan.token('sid', function(req) {
+        return req.sessionID || 'NoSessionID';
+    });
+    // [日期] "请求方法 请求URL 响应码 响应时间 ms HTTP协议版本" "SessionID" 客户端IP 远端用户 响应体大小 "响应头referrer" "浏览器UA"
+    app.use(morgan('[:date[iso]] ":method :url :status :response-time ms - HTTP/:http-version" ":sid" :remote-addr - :remote-user :res[content-length] ":referrer" ":user-agent"', {
+        stream: accessLogStream
+    }));
+} else {
+    app.use(morgan('dev'));
+}
 
 app.use(location());
 app.use('/', routes);
